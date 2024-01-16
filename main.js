e7248ae42b64c5ec38dc9b3d697d09150bf21dd0
daftboston <dsantoyop@gmail.com>
2024-01-16 11:19:27 -0500

    animacion tarjetas

---
+++ main.js
@@ -0,0 +1,13 @@
+const hiddenElements = document.querySelectorAll(".hidden")
+
+
+
+const observer = new IntersectionObserver((entries) => {entries.forEach((entry)=>{if(entry.isIntersecting) {entry.target.classList.add("show")}else{
+    entry.target.classList.remove("show")
+}})},{threshold: 0.5,})
+
+hiddenElements.forEach((element) => observer.observe(element))
+
+
+
+
