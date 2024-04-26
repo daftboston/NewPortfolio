bb2f370e6ca710a3225d294804ad01cfbdbdd4e6
daftboston <dsantoyop@gmail.com>
2024-04-26 14:27:43 -0500

    actualiacion soft scroll

--- main.js
+++ main.js
@@ -2,6 +2,34 @@ import './darkmode.js'
 import {cambiarLenguage} from './language.js'
 
 
+//import Lenis from 'lenis'
+
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+/*
+lenis.on('scroll', ScrollTrigger.update)
+
+gsap.ticker.add((time)=>{
+  lenis.raf(time * 1000)
+})
+
+gsap.ticker.lagSmoothing(0) /**/
+
+
+
+
+
+
 const language = document.querySelector('.language')
 
 
